27a884a760857f4c604ceb3613b50f35d1ede817
yD4rk <68086860+yD4rk@users.noreply.github.com>
2021-02-12 22:11:06 -0300

    Add dynamic color to embed.

--- commands/ping.js
+++ commands/ping.js
@@ -1,7 +1,16 @@
 module.exports = {
   name: 'ping',
   description: 'See my connection to the API',
-  async execute (message, args) {
-    
+  async execute (client, message, args) {
+    let color = '0x0099ff'
+    if (client.ws.ping > 100) color = '0xff0000'
+
+    let Ping = {
+      title: 'Ping:'
+      color: color
+      description: client.ws.ping
+    }
+
+    message.reply({ embed: Ping })
   },
 }
